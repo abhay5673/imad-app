d67a756712e5cdbbcab1953affa33358699dab42
abhay5673 <abhaygirish1997@gmail.com>
2017-08-18 23:10:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles={
-     'articleone':{  title: 'article-abhay deshpande',
+     'article-one':{  title: 'article-abhay deshpande',
     heading: 'article-one',
     date:'18th august 2017',
     content:`   <p>
@@ -24,7 +24,7 @@ var articles={
                   this is my first article ,hello people back with THE MOBILE APP DEVELOPMENT
              </p>`
      },
-     'articletwo': {  title: 'article-abhay deshpande',
+     'article-two': {  title: 'article-abhay deshpande',
     heading: 'article-two',
     date:'18th august 2017',
     content:`   <p>
@@ -42,7 +42,7 @@ var articles={
                   this is my first article ,hello people back with THE MOBILE APP DEVELOPMENT
              </p>`
      },
-     'articlethree': { title: 'article-abhay deshpande',
+     'article-three': { title: 'article-abhay deshpande',
     heading: 'article-three',
     date:'18th august 2017',
     content:`   <p>
