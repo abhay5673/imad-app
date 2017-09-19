928dbf1ba64d7038aa6d7bc38bbb0d00f7846b9d
abhay5673 <abhaygirish1997@gmail.com>
2017-09-19 22:13:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,44 +1,24 @@
 var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-var Pool =require('pg').Pool;
-
-var config= {
-    user:'abhaygirish1997',
-    database: 'abhaygirish1997',
-    host:'db.imad.hasura-app.io',
-    port:'5432',
-    password:process.env.DB.PASSWORD
-};
-
-var app = express();
-app.use(morgan('combined'));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
-
-var pool= new Pool(config);
-app.get('/test-db',function (req,res){
-   pol.query('Select * from test',function(err,result){
-       if(err){
-           res.status(500).send(err,toString());
-       }else{
-           res.send(JSON.stringify(result));
-       }
-   } );
-});
+ var morgan = require('morgan');
+ var path = require('path');
+ 
+ var app = express();
+ app.use(morgan('combined'));
+ 
+ app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+ });
+ 
+ app.get('/ui/style.css', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+ });
+ 
+ app.get('/ui/madi.png', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+ });
+ 
+ 
+ var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+ app.listen(8080, function () {
+   console.log(`IMAD course app listening on port ${port}!`);
+ });
